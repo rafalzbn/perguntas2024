80b08f8a2df1e89b354496f8023a059961466118
RafaZ <132003654+rafalzbn@users.noreply.github.com>
2024-09-26 16:02:53 -0300

    bora la

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Questão 01',
+    'Quanto é 2+3?',
+    'A soma entre 2 e 3 é igual a 5.'
+)
+
+criaCartao(
+    'Questão 02',
+    'A capital do Paraná é?',
+    'A capital do Paraná é Curitiba.'
+    
+)
+
+criaCartao(
+    'Questão 03',
+    'Quem foi que colonizou o Brasil?',
+    'Um páis com o nome de Portugal.'
+)
+
+criaCartao(
+    'Questão 04',
+    'Quem é o Melhor Professor do Universo???',
+    'É O BUYU!!!'
+)
